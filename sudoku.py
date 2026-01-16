796caf252886544205a4fc522ed447663d58df58
Tabakawaa <92632343+Tabakawaa@users.noreply.github.com>
2026-01-17 00:13:02 +0100

    Functions
    Added sqaure_detection function
    Working on new generation function

--- sudoku.py
+++ sudoku.py
@@ -3,18 +3,24 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
+# This function returns squares corner locations [top_left,bottom_left,top_right,bottom_right]
+def square_detection(row, column, game_size):
+    square = [0, 0, 0, 0]
+    row_mod = row % game_size
+    column_mod = column % game_size
+
+    square[0] = row - row_mod  # min x
+    square[1] = column - column_mod  # min y
+    square[2] = square[0] + game_size - 1  # max x
+    square[3] = square[1] + game_size - 1  # max y
+
+    return square
+
 def square_protection(num, j, k, game_matrix, game_size):
     if game_matrix is []:
         return False
     else:
-        square = [0,0,0,0]
-        j_mod = j % game_size
-        k_mod = k % game_size
-
-        square[0] = j - j_mod #min x
-        square[1] = k - k_mod #min y
-        square[2] = square[0] + game_size-1 #max x
-        square[3] = square[1] + game_size-1 #max y
+        square = square_detection(j,k,game_size)
 
 
         rest_empty = False
@@ -22,7 +28,7 @@ def square_protection(num, j, k, game_matrix, game_size):
             for column in range(square[1], square[3]+1):
                 try:
                     if num == game_matrix[row][column]:
-
+                        print(f"Square protection is True for {num} at square {square}")
                         return True
                 except:
                     rest_empty = True
@@ -36,12 +42,14 @@ def column_protection(num, column, game_matrix):
         return False
     for row in game_matrix:
         if num == row[column]:
+            print(f"Column protection is True for {num} at column {column}")
             return True
     else:
         return False
 
 def row_protection(num, rowlist):
     if num in rowlist:
+        print(f"Row protection is True for {num} at row {rowlist}")
         return True
     else:
         return False
@@ -80,6 +88,69 @@ def create_game_matrix_solution(game_size):
 
         game_matrix.append(row)
         j = j+1
+
+    return game_matrix
+
+#TODO: Create updated matrix solution generator
+def create_game_matrix_solution2(game_size):
+    """
+    Creates a matrix by creating new square, then tries fitting the square somewhere in matrix.
+    If not successful deletes square and tries again.
+    :param game_size: number of squares in row or column
+    :return:
+    """
+    size = game_size*game_size
+    game_matrix = []
+
+    #Create new empty matrix
+    for cell_x in range(size):
+        matrix_row = []
+        for cell_y in range(size):
+            matrix_row.append("")
+        game_matrix.append(matrix_row)
+    x = 0
+    y = 0
+    number_of_squares = 0
+    exclude_num = [0]
+    while number_of_squares < size:
+        print(f"I'm inside while number_of_squares!")
+        num_not_in_place = True
+
+        num = random.choice([z for z in range(1, size + 1) if z not in exclude_num])
+        while num_not_in_place:
+            square_location = square_detection(y, x, game_size)
+            print(f"I'm inside while num_not_in_place!")
+
+            if not square_protection(num,y,x,game_matrix,game_size) and not column_protection(num,y,game_matrix) and not column_protection(num,x,game_matrix):
+                print(f"I'm inside if not!")
+                num_not_in_place = False
+                game_matrix[y][x] = num
+                exclude_num.append(num)
+                #TODO Problem is somewhere here. when square whats to do second (1)
+                # row in second square it jumps back into the first square
+                if y == square_location[2] and x == square_location[3]:
+                    number_of_squares = number_of_squares + 1
+                    if square_location[2] == size - 1:
+                        y = square_location[2] + 1
+                        x = 0
+                        exclude_num = [0]
+
+                    else:
+                        y = square_location[1]
+                        x = square_location[3] + 1
+                        exclude_num = [0]
+                # Might seem like a stupid idea, but not exclude_num == [0]
+                # actually tells this function if we want to change square or not
+                if x == square_location[3] and not exclude_num == [0]:
+                    x = 0
+                    y = y + 1
+                elif not exclude_num == [0]:
+                    x = x + 1
+            else:
+                # We are not excluding the not suitable number because it could work on another row or column
+                #TODO: Add special exclude list for each protection
+                num = random.choice([z for z in range(1, size + 1) if z not in exclude_num])
+
 
     return game_matrix
 
@@ -172,15 +243,7 @@ if __name__ == '__main__':
     GAME_SIZE = 3
     game_mode = "medium" #easy, medium, hard
 
-    solution = create_game_matrix_solution(GAME_SIZE)
-    for i in range(len(solution)):
-        print(solution[i])
+    solution = create_game_matrix_solution2(GAME_SIZE)
+    print(solution)
 
-    print(" ")
-
-    game = create_game_matrix(solution, game_mode)
-    for i in range(len(game)):
-        print(game[i])
-
-    show_game_matrix(solution, GAME_SIZE)
 
